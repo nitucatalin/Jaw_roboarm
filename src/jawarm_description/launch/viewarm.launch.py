872d3d19a36b4789765e2e1676475764513c820b
nitucatalin <nitu.george15@gmail.com>
2025-09-24 14:39:34 +0300

    urdf update

--- src/jawarm_description/launch/viewarm.launch.py
+++ src/jawarm_description/launch/viewarm.launch.py
@@ -1,28 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
 
 def generate_launch_description():
-    urdf_file = os.path.join(
-        get_package_share_directory('jawarm_description'),
-        'urdf',
-        'jaw.urdf'
-    )
+    urdf_file='/home/ktl/Jaw_roboarm/src/jawarm_description/urdf/jaw.urdf'
+    
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
-            executable='root_state_publisher',
+            executable='robot_state_publisher',
             name='robot_state_publisher',
-            output='screen'
-            arguments=[urdf_file]
+            output='screen',
+            parameters=[{"use_sim_time": False, "robot_description": open("src/jawarm_description/urdf/jaw.urdf").read()}]
         ),
-
+        Node(
+            package="joint_state_publisher_gui",
+            executable="joint_state_publisher_gui",
+            name="joint_state_publisher_gui",
+            output="screen"
+        ),
         Node(
             package='rviz2',
             executable='rviz2',
-            name='rvzi2',
-            output='screen'
-            arguments=['-d', os.path.join(get_package_share_directory('jawarm_description'), 'rviz', 'view_robot.rviz')]
+            name='rviz2',
+            output='screen',
+            arguments=['-d', '/home/ktl/Jaw_roboarm/src/jawarm_description/launch/viewarm.rviz']
         )
     ])
